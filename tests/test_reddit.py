3a29a44babd0961ef0af9eeac74736a883956941
Christopher Sardegna <github@reagentx.net>
2019-05-10 14:03:21 -0700

    Create test_reddit.py

---
+++ tests/test_reddit.py
@@ -0,0 +1,16 @@
+import os
+import pathlib
+import unittest
+from reddit_api import submit
+
+
+class TestRedditMethods(unittest.TestCase):
+
+    def test_submit(self):
+        submission = submit.post('test', 'title test', 'https://test.com')
+        self.assertIsNotNone(submission)
+        submission.delete()
+
+
+if __name__ == "__main__":
+    unittest.main()
