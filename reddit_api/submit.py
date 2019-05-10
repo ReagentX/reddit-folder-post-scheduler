ce1eb1acec0b64107a3236231667d2d479591bb0
Christopher Sardegna <github@reagentx.net>
2019-05-10 14:03:29 -0700

    Retry failed sumbissions with sleep

--- reddit_api/submit.py
+++ reddit_api/submit.py
@@ -1,14 +1,24 @@
 import praw
+import time
 
 
 reddit = praw.Reddit('bot')
 
 
 def post(sub, title, url):
-    result = reddit.subreddit(sub).submit(
-        title,
-        url=url
-    )
+    """Submit a post, retry up to 3 times"""
+    retries = 0
+    while retries < 3:
+        try:
+            result = reddit.subreddit(sub).submit(
+                title,
+                url=url
+            )
+        except:
+            print('Rate limited, retrying in 10 minutes')
+            time.sleep(10 * 60)  # 10 minutes
+            retries += 1
+
     if result: 
-        return submission.permalink
+        return submission
     return None
