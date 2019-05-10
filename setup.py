8c3561b083fe51d75aa3ddff0d8539e6e5b2f023
Christopher Sardegna <github@reagentx.net>
2019-05-10 12:46:04 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='reddit-folder-post-scheduler',
+    version='1.0',
+    description='A Python 3 API to schedule image posts from a folder of images',
+    author='Christopher Sardegna',
+    author_email='github@reagentx.net',
+    install_requires=['requests'],
+    packages=find_packages(),
+    scripts=[]
+)
