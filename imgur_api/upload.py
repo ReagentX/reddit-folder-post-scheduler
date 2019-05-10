27304a64bb21b403a7fca57f1dc0afebbde6867e
Christopher Sardegna <github@reagentx.net>
2019-05-10 13:22:29 -0700

    Get anon upload working

--- imgur_api/upload.py
+++ imgur_api/upload.py
@@ -1,4 +1,30 @@
+import json
 import requests
 
 API_ROOT = 'https://api.imgur.com/3/'
 
+class ImgurAPI():
+    def __init__(self, client_id):
+        client_id = client_id
+        self.headers = {
+            'Authorization': f'Client-ID {client_id}'
+        }
+
+
+    def post_image(self, image, title):
+        body = {
+            'image': image,
+            'type': 'file',
+            'name': title,
+            'title': title,
+            'description': '',
+            'disable_audio': 1
+        }
+
+        url = f'{API_ROOT}/upload'
+        response = requests.post(url, data=body, headers=self.headers)
+        print(response)
+        if response.json():
+            return response.json()['data']['link']
+        else:
+            return None
--- tests/test_imgur.py
+++ tests/test_imgur.py
@@ -1,11 +1,21 @@
+import os
+import pathlib
 import unittest
 from imgur_api import upload
 
 
 class TestImgurMethods(unittest.TestCase):
 
+    imgur = upload.ImgurAPI('')
+    base_path = pathlib.Path('tests/')
+
+
     def test_upload(self):
-        pass
+        with open(f'{self.base_path}/image.png', "rb") as f:
+            image = f.read()
+        link = self.imgur.post_image(image, 'test post')
+        self.assertIsNotNone(link)
+
 
 if __name__ == "__main__":
     unittest.main()
