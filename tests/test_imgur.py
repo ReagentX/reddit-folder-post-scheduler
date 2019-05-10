2d1a69e450911dd7459daf243f800d0022da91d6
Christopher Sardegna <github@reagentx.net>
2019-05-10 12:46:11 -0700

    Get test scaffolding set up

--- tests/test_imgur.py
+++ tests/test_imgur.py
@@ -1,2 +1,11 @@
 import unittest
 from imgur_api import upload
+
+
+class TestImgurMethods(unittest.TestCase):
+
+    def test_upload(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
