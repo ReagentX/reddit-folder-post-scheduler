c4f5a9eddc7f33438cad95cd84caf4f2d01f69cb
Christopher Sardegna <github@reagentx.net>
2019-05-12 10:58:15 -0700

    Add docstring

--- reddit_api/submit.py
+++ reddit_api/submit.py
@@ -7,7 +7,7 @@ reddit = praw.Reddit('bot')
 
 
 def post(sub, title, url):
-    """Submit a post, retry up to 3 times"""
+    """Submit a post to Reddit, retry up to 3 times"""
     retries = 0
     while retries < 3:
         try:
@@ -18,6 +18,7 @@ def post(sub, title, url):
             break
         except:
             print('Rate limited!')
+            # Countdown MM:SS
             for remaining in range(10 * 60, 0, -1):
                 minutes, seconds = divmod(remaining, 60)
                 sys.stdout.write("\r")
