d96bb297b8f8d4a320d3ba3f0b174e7b22732863
Christopher Sardegna <github@reagentx.net>
2019-05-12 19:55:36 -0700

    Simplify imgur test

--- tests/test_imgur.py
+++ tests/test_imgur.py
@@ -7,13 +7,12 @@ from imgur_api import upload
 
 class TestImgurMethods(unittest.TestCase):
 
-    imgur = upload.ImgurAPI('')
-
 
     def test_upload(self):
         """Upload a sample image to Imgur, make sure we get a link"""
+        imgur = upload.ImgurAPI('')
         image_path = pathlib.Path('tests/image.png')
-        link = self.imgur.post_image(image_path, 'test post')
+        link = imgur.post_image(image_path, 'test post')
         self.assertIsNotNone(link)
 
 
