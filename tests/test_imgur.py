8eb6fe17e5a6c95646dd59a43b11f1811650ee74
Christopher Sardegna <github@reagentx.net>
2019-05-12 19:52:25 -0700

    Add comments to tests

--- tests/test_imgur.py
+++ tests/test_imgur.py
@@ -10,6 +10,7 @@ class TestImgurMethods(unittest.TestCase):
 
 
     def test_upload(self):
+        """Upload a sample image to Imgur, make sure we get a link"""
         image_path = pathlib.Path('tests/image.png')
         link = self.imgur.post_image(image_path, 'test post')
         self.assertIsNotNone(link)
--- tests/test_reddit.py
+++ tests/test_reddit.py
@@ -7,6 +7,7 @@ from reddit_api import submit
 class TestRedditMethods(unittest.TestCase):
 
     def test_submit(self):
+        """Submit a test post and delete"""
         submission = submit.post('test', 'title test', 'https://test.com')
         self.assertIsNotNone(submission)
         submission.delete()
